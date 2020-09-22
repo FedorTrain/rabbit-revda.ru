27efb8f16b58f3dc61157a8c50bc7f1d1cbaa2e7
FedorTrain <fedya_dryagin@mail.ru>
2020-09-22 10:24:23 +0500

    4

--- php/logup.php
+++ php/logup.php
@@ -3,7 +3,7 @@
   $username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
   $password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
 
-  $mysql = new mysqli('localhost', 'u1088104_default', ' 1!nQ9occ', 'u1088104_users');
+  $mysql = new mysqli('localhost', 'u1088104', 'Q8p7P2s5', 'u1088104_users');
   $mysql->query("INSERT INTO `info` (`username`, `password`)
   VALUES('$username','$password')");
 
