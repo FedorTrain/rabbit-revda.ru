8f6c94cd0ab19f6428de6cef9f5726ddd3af2151
FedorTrain <fedya_dryagin@mail.ru>
2020-09-22 10:11:33 +0500

    try 2

--- php/logup.php
+++ php/logup.php
@@ -3,11 +3,11 @@
   $username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
   $password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
 
-  $mysql = new mysqli('localhost', 'u1088104_users', 'Q8p7P2s5', 'info');
-  $mysql->query("INSERT INTO `products` (`name`, `price`, `image`, `info`, `type`, `number`)
-  VALUES('$name','$price','$image','$info','$type','$number')");
+  $mysql = new mysqli('localhost', 'u1088104_admin', 'Q8p7P2s5', 'u1088104_users');
+  $mysql->query("INSERT INTO `info` (`username`, `password`)
+  VALUES('$username','$password')");
 
   $mysql->close();
-  header('Location:/admin');
+  // header('Location:/admin');
 
 ?>
