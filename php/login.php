976b3c36b31d9b55578df1736ae05dadc6f16db4
FedorTrain <fedya_dryagin@mail.ru>
2020-09-22 10:56:25 +0500

    3

--- php/login.php
+++ php/login.php
@@ -4,11 +4,10 @@
   $password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
 
   $mysql = new mysqli('localhost', 'u1088104', 'Q8p7P2s5', 'u1088104_users');
-  $result = $mysql->query("SELECT * FROM `info` WHERE 'username' = $username and 'password' = $password");
+  $user = $mysql->query("SELECT * FROM `info` WHERE 'username' = $username and 'password' = $password");
 
   $mysql->close();
   // header('Location:/admin');
-  $user = $result->fetch_assoc();
 
   echo json_encode($user);
 
