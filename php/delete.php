30570ccf0392253b049ac7ea0d75dc15bcbd3d45
FedorTrain <fedya_dryagin@mail.ru>
2020-09-22 18:54:13 +0500

    9

--- php/delete.php
+++ php/delete.php
@@ -1,36 +1,22 @@
-<!DOCTYPE html>
-<html >
-  <head>
-    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="utf-8" http-equiv="encoding">
-    <title>SoftPajamas</title>
-  </head>
-  <body>
-    <div id="content">
-      <?php
-        $pass = $_POST['pass'];
-        if ($pass == "dickduck") {
+<?php
+  $pass = $_POST['pass'];
+  if ($pass == "dickduck") {
 
-          $id = $_POST['id'];
-          $mysql = new mysqli('localhost', 'u1088104', 'Q8p7P2s5', 'u1088104_users');
-          $result = $mysql->query("SELECT * FROM `product` WHERE `id` = $id");
-          $mysql->query("DELETE FROM products WHERE id = $id");
-          $mysql->close();
-          $product = $result->fetch_assoc();
+    $id = $_POST['id'];
+    $mysql = new mysqli('localhost', 'u1088104', 'Q8p7P2s5', 'u1088104_users');
+    $result = $mysql->query("SELECT * FROM `product` WHERE `id` = $id");
+    $mysql->query("DELETE FROM products WHERE id = $id");
+    $mysql->close();
+    $product = $result->fetch_assoc();
 
-          $filename = $product['image'];
+    $filename = $product['image'];
 
-          unlink('/var/www/u1088104/data/www/rabbit-revda.ru/images/' . $filename);
+    unlink('/var/www/u1088104/data/www/rabbit-revda.ru/images/' . $filename);
 
-        }
-        else {
-          echo "password is bad";
-        }
-        header('Location:/admin');
+  }
+  else {
+    echo "password is bad";
+  }
+  header('Location:/admin');
 
-      ?>
-
-    </div>
-
- </body>
-</html>
+?>
